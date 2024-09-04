8816ff81855508056493cc169091407877bd380c
BenutzerEinsZweiDrei <57397036+BenutzerEinsZweiDrei@users.noreply.github.com>
2024-09-04 15:46:52 +0200

    Update script.js

--- MdRender/script.js
+++ MdRender/script.js
@@ -17,28 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     const sanitizedHTML = DOMPurify.sanitize(marked.parse(decodeURIComponent(markdownContent)));
-    document.getElementById("content").innerHTML = sanitizedHTML;
+    document.getElementById("mdhere").innerHTML = sanitizedHTML;
 
-    executeInlineScripts(); // Execute any inline scripts that were sanitized
+    // executeInlineScripts(); // Execute any inline scripts that were sanitized
     setLinksToOpenInNewTab(); // Make sure all links open in a new tab
-  }
-
-  function executeInlineScripts() {
-    const scriptTags = document.querySelectorAll("#content script");
-    scriptTags.forEach(function (scriptTag) {
-      if (scriptTag.src) {
-        const externalScript = document.createElement("script");
-        externalScript.src = scriptTag.src;
-        document.getElementById("content").removeChild(scriptTag);
-        document.getElementById("content").appendChild(externalScript);
-      } else {
-        try {
-          eval(scriptTag.innerText);
-        } catch (error) {
-          console.log("Error executing script:", error);
-        }
-      }
-    });
   }
 
   function setLinksToOpenInNewTab() {
@@ -56,45 +38,3 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("No markdown content provided.");
   }
 });
-
-// Additional utility functions remain unchanged
-var findBlocks = function (data, variableNames) {
-  const matches = [];
-  variableNames.forEach(function (variable) {
-    const regexPattern = `#region(?<variableName> ${variable})(?<content>[\\s\\S]*?)(#endregion)`;
-    let regex = new RegExp(regexPattern, "g");
-    for (const match of data.matchAll(regex)) {
-      const variableName = match.groups.variableName.trim();
-      if (variableNames.includes(variableName)) {
-        const content = match.groups.content.trim();
-
-        matches.push({
-          variableName,
-          content,
-        });
-      }
-    }
-  });
-  return matches;
-};
-
-function showBlocks(data, variableNames) {
-  var blocks = findBlocks(data, variableNames);
-  blocks.forEach(function (item, index) {
-    let variableNameBlock = document.getElementById(item.variableName);
-    let codeBlock = document.getElementById("code" + index);
-    if (codeBlock !== null) {
-      codeBlock.textContent = item.content;
-      hljs.highlightElement(codeBlock);
-    }
-    if (variableNameBlock !== null) {
-      variableNameBlock.textContent = item.variableName);
-    }
-  });
-}
-
-function handleDocumentWrite(content) {
-  var contentPlaceholder = document.getElementById("content");
-  contentPlaceholder.innerHTML += content;
-}
-window.document.write = handleDocumentWrite;
